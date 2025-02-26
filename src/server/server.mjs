4d54c3a5f007fbeb2e8ac51e1f42bf7163d63e62
zzy <1061342204@qq.com>
2025-02-26 10:21:22 +0800

    add:dotenv

--- src/server/server.mjs
+++ src/server/server.mjs
@@ -1,12 +1,19 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors'
+import dotenv from 'dotenv';
+
+// 加载 .env 文件
+dotenv.config();
+const PORT = process.env.SERVER_PORT
 
 const app = express();
 
 // 设置静态文件服务，指向 public 目录
 app.use(express.static(path.join('..', '..', 'public')));
+app.use(cors)
 
 // 启动服务器
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${import.meta.env.PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
+import dotenv from 'dotenv';
+
+// 加载 .env 文件
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), UnoCSS()],
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${process.env.SERVER_PORT}`,
+        changeOrigin: true,
+      },
+    },
+  },
 })
