f5f799085dcaeb0a8254cbf2e6d6ea9a93be9018
zzy <1061342204@qq.com>
2025-02-26 09:25:34 +0800

    add:pinia

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+const app = createApp(App)
+
 // 导入 pdfjs 并设置工作目录
 import * as pdfjsLib from 'pdfjs-dist';
 pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
     'pdfjs-dist/build/pdf.worker.js',
     import.meta.url
-  ).toString();  // [3,9](@ref)
+).toString();  // [3,9](@ref)
+
+// 导入 pinia
+import { createPinia } from 'pinia'
+const pinia = createPinia()
+app.use(pinia)
 
 createApp(App).mount('#app')
