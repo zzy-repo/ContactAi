2bd2c5400b8d9f5d6ec19123524b8c238a255b6d
zzy <1061342204@qq.com>
2025-02-26 10:32:38 +0800

    fix:file path

--- src/server/server.mjs
+++ src/server/server.mjs
@@ -2,6 +2,13 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors'
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url'; // 新增：解决 ESM 模块下 __dirname 的兼容问题
+
+// 获取当前文件的绝对路径（ESM 模块替代 __dirname）
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __publicpath = path.join(__dirname, '..', '..', 'public')
+// console.log(__publicpath)
 
 // 加载 .env 文件
 dotenv.config();
@@ -10,7 +17,7 @@ const PORT = process.env.SERVER_PORT
 const app = express();
 
 // 设置静态文件服务，指向 public 目录
-app.use(express.static(path.join('..', '..', 'public')));
+app.use(express.static(__publicpath))
 app.use(cors())
 
 // 启动服务器
